44daeb05f9ab5ed73ab26664ff4fe69c39b19116
Bryan Valarezo <bvalarezo@cse376u18.cs.stonybrook.edu>
2020-02-04 15:29:09 -0500

    added getopt to the main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
-int main(int argc, char const *argv[])
+#define ENCRYPT 'e'
+#define DECRYPT 'd'
+#define VERSION 'v'
+#define HELP 'h'
+#define DBGVAL 'D'
+#define PASSFILE 'p'
+
+#define VERSION_NUMBER 1.0
+
+static char *options = "devhD:p:";
+
+int main(int argc, char *argv[])
 {
+    int opt;
+    while ((opt = getopt(argc, argv, options)) != -1)
+    {
+        switch (opt)
+        {
+        case ENCRYPT:
+            printf("Encrypt\n");
+            break;
+
+        default:
+            break;
+        }
+    }
     printf("Hello World\n");
     return 0;
 }
